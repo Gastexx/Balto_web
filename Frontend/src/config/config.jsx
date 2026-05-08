85f76c6030d34b59b352074219d99e6cab1e7702
Bruno Ballarino <brunoballarino06@gmail.com>
2026-05-08 12:56:47 -0300

    cm

--- Frontend/src/config/config.jsx
+++ Frontend/src/config/config.jsx
@@ -1,3 +1,10 @@
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3001/routes";
+const BASE_URL = process.env.REACT_APP_API_URL || "https://panel.balto.com.ar/api/routes";
 
 export default BASE_URL;
+
+
+
+//Hostinger produccion: https://panel.balto.com.ar/api/routes
+
+//Local: http://localhost:3001/routes
+
